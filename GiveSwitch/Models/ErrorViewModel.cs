098c22886f6db03ce38ab13275d99aca1fd9d58a
Burt Fous <burtfous@gmail.com>
2018-12-19 22:23:58 -0800

    Add project files.

---
+++ GiveSwitch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GiveSwitch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
